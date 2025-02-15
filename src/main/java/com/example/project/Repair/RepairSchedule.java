d60c9523bff4b7326498c56141603c2d46830dc5
benjamins266 <benjamins266@nycstudents.net>
2025-02-15 15:28:05 +0000

    Binary Search

--- src/main/java/com/example/project/Repair/RepairSchedule.java
+++ src/main/java/com/example/project/Repair/RepairSchedule.java
@@ -13,6 +13,8 @@ public class RepairSchedule {
      */
     public RepairSchedule(int n) {
         /* to be implemented in part (a) */
+        numberOfMechanics = n;
+        schedule = new ArrayList<>();
     }
 
     public ArrayList<CarRepair> getSchedule() {
@@ -23,6 +25,12 @@ public class RepairSchedule {
      * Precondition: 0 <= m < numberOfMechanics and b >= 0
      */
     public boolean addRepair(int m, int b) {
+        for(int i = 0; i<schedule.size(); i++){
+            if(schedule.get(i).getMechanicNum() == m || schedule.get(i).getBayNum() == b){
+                return false;
+            }
+        }
+        schedule.add(new CarRepair(m, b));
         return true;
     }
 
@@ -30,8 +38,22 @@ public class RepairSchedule {
      * as described in part (b).
      */
     public ArrayList<Integer> availableMechanics() {
-        return new ArrayList<Integer>();
-    }
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(int i = 0; i<numberOfMechanics; i++){
+            boolean available = true;
+            for(int j = 0; j<schedule.size(); j++){
+                if(schedule.get(j).getMechanicNum() == i){
+                    available = false;
+                    break;
+                }
+            }
+            if(available == true){
+                nums.add(i);
+            }
+            }
+            return nums;
+        }
+
 
     /** Removes an element from schedule when a repair is complete.
         THIS METHOD HAS BEEN ADDED FOR ILLUSTRATIVE PURPOSES ONLY,
--- src/main/java/com/example/project/Spell_Checker/SpellChecker.java
+++ src/main/java/com/example/project/Spell_Checker/SpellChecker.java
@@ -50,8 +50,30 @@ public class SpellChecker {
      *  prints that value out before returning.
      */
     public boolean binarySpellCheck(String word) {
-        return false;
-    }
+        int leftIdx = 0;    // TODO: assign this initial value
+        int rightIdx = dictionary.size()-1;  // TODO: assign this initial value
+
+        while (leftIdx<=rightIdx) {  // TODO: determine this condition (hint: see slides 61-63)
+            loopCounter++; // for testing
+            System.out.print(loopCounter + " "); // for testing
+            int middleIdx = ((leftIdx + rightIdx) /2);   // TODO: determine what this should be
+
+            // TODO: write the rest of the code to compare middleIdx to the target
+            //  and adjust leftIdx and rightIdx as appropriate (see slides if needed)
+
+            if(dictionary.get(middleIdx).equals(word)){
+                System.out.println("-- BINARY SEARCH: Number of words checked (loop iterations): " + loopCounter);
+                return true;
+            } else if(dictionary.get(middleIdx).compareTo(word)>0){
+                    rightIdx = middleIdx - 1;
+            } else {
+                    leftIdx = middleIdx + 1;
+                }
+            }
+            System.out.println("-- BINARY SEARCH: Number of words checked (loop iterations): " + loopCounter);
+            return false;
+        }
+
 
     // private helper method, called in the constructor, which loads the words
     // from the dictionary.txt text file into the "dictionary" instance variable!
